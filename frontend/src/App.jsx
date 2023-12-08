793d671bc87c150d0bc7d67a9b123984f141548c
Umair Khan <81556052+i-umairkhan@users.noreply.github.com>
2023-12-08 11:00:39 +0000

    created frontemd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+
 
 function App() {
 
   return (
     <>
-    Umair Khan
+      <h1>News Ranking Tool</h1>
+      <h3>This tool is trained on DAWN News dataset.</h3>
+      <TextField
+        label="Search Query"
+        sx={{
+          marginTop: "20px",
+          marginRight: "20px",
+        }}
+      />
+      <Button variant="contained"
+        sx={{
+          marginTop: "20px",
+          marginRight: "20px",
+          padding: "15px"
+        }}
+      >Search</Button>
     </>
   )
 }
