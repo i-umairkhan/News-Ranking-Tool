49a973d9958243463feb31c087229bb6c7027b21
Umair Khan <81556052+i-umairkhan@users.noreply.github.com>
2023-12-08 12:30:08 +0000

    implemented "/query" route

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
 import express from "express"
 import morgan from "morgan"
+import bodyParser from "body-parser"
 
 const app = express()
 const PORT =  3000
 
 app.use(morgan("tiny"))
+app.use(bodyParser.json())
+
 
 app.get("/" , (req, res) => {
     res.send("News Ranking Tool Api")
 })
 
 app.post("/query", (req,res) => {
-    const data = req.body.data;
+    const {data} = req.body;
     res.send(data)
 })
 
