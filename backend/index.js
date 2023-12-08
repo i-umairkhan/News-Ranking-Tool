7f8572c1a32bacfd45e79f3bf761002ce709af39
Umair Khan <81556052+i-umairkhan@users.noreply.github.com>
2023-12-08 13:13:28 +0000

    created python binding

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
+import { spawn } from "child_process"
 import express from "express"
 import morgan from "morgan"
 import bodyParser from "body-parser"
+
 
 const PORT = 3000;
 
@@ -12,16 +14,34 @@ app.use(bodyParser.json());
 
 
 // Routes
-app.get("/" , (req, res) => {
+app.get("/", (req, res) => {
     res.send("News Ranking Tool Api");
 })
 
-app.post("/query", (req,res) => {
-    const {data} = req.body;
+app.post("/query", (req, res) => {
+    const { data } = req.body;
+
+    const filePath = "/workspaces/News-Ranking-Tool/backend/main.py"
+    const pythonProcess = spawn("python",[filePath,data]);
+    pythonProcess.stdout.on('data', (data) => {
+        process.stdout.write(data);
+        console.log(data,"")
+    })
+    pythonProcess.stderr.on('data', (data) => {
+        console.log(data);
+    })
+    pythonProcess.on('close', (code) => {
+        if (code !== 0) {
+            console.error(code);
+        } else {
+            console.log(code);
+        }
+    })
+
     res.send(data);
 })
 
 // Server
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`Running on port: ${PORT}`);
 })
